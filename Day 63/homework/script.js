43cbd32af0faa1547f49bfdf7a65a0e82f7d7730
lukamskh <mskhaladze.luka7@students.gov.ge>
2024-10-06 19:02:08 +0400

    commit

--- Day 63/homework/script.js
+++ Day 63/homework/script.js
@@ -81,3 +81,20 @@ function sum_of_numbers(numbers){
     alert(ans)
 }
 sum_of_numbers([1,2,3,4,5,6,7,8,9])
+
+function filter(numberArr){
+    evenNumbers = []
+    oddNumbers = []
+    for (let num in numberArr){
+        if (num % 2 == 0){
+            evenNumbers += num
+        } else {
+            oddNumbers += num
+        }
+    }
+    console.log(evenNumbers)
+    console.log(oddNumbers)
+}
+
+filter([1,2,3,4,5,6,7,8,9,10])
+// 100
